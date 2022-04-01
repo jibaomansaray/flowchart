0d7778bfbe31ea573a685be1c717e0810230ad92
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-01 15:32:23 +0000

    Remove `fill` and `fillStyle` data
    They have been moved to the fillable component

--- src/ecs/components/circle.ts
+++ src/ecs/components/circle.ts
@@ -5,16 +5,12 @@ export class CircleComponent implements IComponent {
   private _startAngle: number;
   private _endAngle: number;
   private _counterClockwise: boolean;
-  private _fill: boolean;
-  private _fillStye: string;
 
-  constructor(radius = 0, startAngle = 0, endAngle = Math.PI * 2, counterClockwise = true, fill = false, fillStyle = 'black') {
+  constructor(radius = 0, startAngle = 0, endAngle = Math.PI * 2, counterClockwise = true) {
     this._radius = radius;
     this._startAngle = startAngle;
     this._endAngle = endAngle;
     this._counterClockwise = counterClockwise;''
-    this._fill = fill;
-    this._fillStye = fillStyle;
   }
 
   get type(): ComponentTypes {
@@ -51,31 +47,12 @@ export class CircleComponent implements IComponent {
   set counterClockwise(v: boolean) {
     this._counterClockwise = v;
   }
-
-  get fill() {
-    return this._fill;
-  }
-
-  set fill(f: boolean) {
-    this._fill = f;
-  }
-
-  get fillStyle() {
-    return this._fillStye;
-  }
-
-  set fillStyle(style: string) {
-    this._fillStye = style;
-  }
-
   toJSON(): { [key: string]: any; } {
     return {
       radius: this.radius,
       startAngle: this.startAngle,
       endAngle: this.endAngle,
       counterClockwise: this.counterClockwise,
-      fill: this.fill,
-      fillStyle: this.fillStyle,
     };
   }
 
@@ -85,15 +62,11 @@ export class CircleComponent implements IComponent {
       startAngle: number,
       endAngle: number,
       counterClockwise: number,
-      fill: boolean,
-      fillStyle: string,
     }; 
 
     this.radius = json.radius;
     this.startAngle = json.startAngle;
     this.endAngle = json.endAngle;
     this.counterClockwise = json.counterClockwise;
-    this.fill = json.fill;
-    this.fillStyle = json.fillStyle;
   }
 }

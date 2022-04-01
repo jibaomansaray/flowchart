5442b0779e5a86534c37c20ec3809f19726128d8
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-01 14:24:22 +0000

    Create `Circle` entity

---
+++ src/ecs/entities/circle_entity.ts
@@ -0,0 +1,15 @@
+import { Entity } from ".";
+import { CircleComponent } from "../components/circle";
+import { Position } from "../components/position";
+import { EntityType } from "./entity_types";
+
+export class CircleEntity extends Entity {
+
+  constructor(id?: string, type = EntityType.CIRCLE) {
+    super(type, id);
+
+    this.add(new Position());
+    this.add(new CircleComponent());
+  }
+
+}

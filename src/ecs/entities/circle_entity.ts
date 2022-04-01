62ef29b39a24cab6f134d352f12301d54647d3ae
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-01 15:34:08 +0000

    Make circles fillables

--- src/ecs/entities/circle_entity.ts
+++ src/ecs/entities/circle_entity.ts
@@ -1,5 +1,6 @@
 import { Entity } from ".";
 import { CircleComponent } from "../components/circle";
+import { FillableComponent } from "../components/fillable";
 import { Position } from "../components/position";
 import { EntityType } from "./entity_types";
 
@@ -8,6 +9,7 @@ export class CircleEntity extends Entity {
   constructor(id?: string, type = EntityType.CIRCLE) {
     super(type, id);
 
+    this.add(new FillableComponent());
     this.add(new Position());
     this.add(new CircleComponent());
   }

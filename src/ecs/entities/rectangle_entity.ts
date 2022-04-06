23232e1e20030ca59bc832558c9e70ba9c494491
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-06 13:18:15 +0000

    Add more components to the rectangle entity
    - MouseCollisionComponent
    - DrawableComponent
    - InteractComponent

--- src/ecs/entities/rectangle_entity.ts
+++ src/ecs/entities/rectangle_entity.ts
@@ -3,6 +3,9 @@ import { EntityType } from "./entity_types";
 import { Position } from "../components/position";
 import { FillableComponent } from "../components/fillable";
 import { SizeComponent } from "../components/size";
+import { MouseCollisionComponent } from "../components/mouse_collision";
+import { DrawableComponent } from "../components/drawable";
+import { InteractComponent } from "../components/interact";
 
 export class RectangleEntity extends Entity {
 
@@ -11,5 +14,8 @@ export class RectangleEntity extends Entity {
     this.add(new Position());
     this.add(new FillableComponent());
     this.add(new SizeComponent());
+    this.add(new MouseCollisionComponent());
+    this.add(new DrawableComponent());
+    this.add(new InteractComponent());
   }
 }

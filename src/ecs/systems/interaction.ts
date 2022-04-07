989d83c78bde642a2914364c099115765a5cf6a7
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-07 12:25:03 +0000

    Add the ability to toggle the surrounding `box`

--- src/ecs/systems/interaction.ts
+++ src/ecs/systems/interaction.ts
@@ -5,6 +5,9 @@ import { InteractComponent } from "../components/interact";
 import { MouseCollisionComponent } from "../components/mouse_collision";
 import { IEntity } from "../types/entity_types";
 import { ISystem } from "../types/system_types";
+import { Position } from "../components/position";
+import { SizeComponent } from "../components/size";
+import { CircleComponent } from "../components/circle";
 
 export class Interaction implements ISystem {
 
@@ -26,20 +29,61 @@ export class Interaction implements ISystem {
     const drawable = components.get(ComponentTypes.DRAWABLE)! as DrawableComponent;
     const interact = components.get(ComponentTypes.INTERACT)! as InteractComponent;
 
+
     ent.get(ComponentTypes.FILLABLE, (com) => {
       const c = com as FillableComponent;
       if (mouseComponent.detected) {
+        this.toggleBox(true, components, ent);
         drawable.tempStrokeStyle = interact.strokeStyle;
         if (c.fill) {
           c.tempFillStyle = interact.fillStyle;
         }
       } else {
+        this.toggleBox(false, components, ent);
         drawable.tempStrokeStyle = '';
         if (c.fill) {
           c.tempFillStyle = ''
         }
       }
     })
+  }
+
+  private toggleBox(detected: boolean, components: Map<ComponentTypes, IComponent>, ent: IEntity) {
+    const interact = components.get(ComponentTypes.INTERACT)! as InteractComponent;
+    const pos = ent.get(ComponentTypes.POSITION)! as Position;
+    const size = ent.get(ComponentTypes.SIZE)! as SizeComponent;
+    const circle = ent.get(ComponentTypes.CIRCLE)! as CircleComponent;
+
+    if (detected) {
+      interact.box.get(ComponentTypes.POSITION, (c) => {
+        const p = c as Position;
+        if (circle) {
+          p.x = pos.x - circle.radius - 4;
+          p.y = pos.y - circle.radius - 4
+        } else {
+          p.x = pos.x - 4;
+          p.y = pos.y - 4
+        }
+      });
+
+      interact.box.get(ComponentTypes.SIZE, (c) => {
+        const s = c as SizeComponent;
+        if (circle) {
+          s.width = circle.width + 10;
+          s.height = circle.height + 10;
+        } else {
+          s.width = size.width + 8;
+          s.height = size.height + 8;
+        }
+      });
+    } else {
+      interact.box.get(ComponentTypes.SIZE, (c) => {
+        const s = c as SizeComponent;
+        s.width = 0
+        s.height = 0;
+      });
+    }
+
   }
 }
 

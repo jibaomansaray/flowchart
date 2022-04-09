2de26be822000379a0891165a25d81b67e6f409c
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:25:03 +0000

    Add return type to the dection methods

--- src/ecs/systems/mouse_collision_detection.ts
+++ src/ecs/systems/mouse_collision_detection.ts
@@ -10,43 +10,69 @@ import { ISystem } from "../types/system_types";
 export class MouseCollisionDetection implements ISystem {
 
   private mouseOver: boolean = false;
+  private mouseDown: boolean = false;
   private x: number = -1;
   private y: number = -1;
+  private entityId = '';
+  private _offsetX: number = 0;
+  private _offsetY: number = 0;
 
   setup(canvas: HTMLCanvasElement): void {
+    this._offsetX = canvas.getBoundingClientRect().left;
+    this._offsetY = canvas.getBoundingClientRect().top;
+
     // handle mouse move 
     canvas.addEventListener('mousemove', (event: MouseEvent) => {
       this.mouseOver = true;
-      this.x = event.offsetX;
-      this.y = event.offsetY;
+      this.x = event.clientX - this._offsetX;
+      this.y = event.clientY - this._offsetY;
     });
 
 
     canvas.addEventListener('mouseleave', (_event: MouseEvent) => {
       this.mouseOver = false;
-      this.x = -1;
-      this.y = -1;
+      this.entityId = '';
+      this.mouseDown = false;
+      // this.x = -1;
+      // this.y = -1;
+    });
+
+    canvas.addEventListener('mousedown', () => {
+      this.mouseDown = true;
+    });
+
+    canvas.addEventListener('mouseup', () => {
+      this.mouseDown = false;
+      this.entityId = '';
     });
   }
 
   update(entity: IEntity, _ctx: CanvasRenderingContext2D, _timestamp: number): void {
-    if (this.mouseOver) {
+    if (this.mouseOver && !this.entityId) {
       const circle = [ComponentTypes.POSITION, ComponentTypes.CIRCLE, ComponentTypes.MOUSE_COLLISION];
       const rectangle = [ComponentTypes.POSITION, ComponentTypes.SIZE, ComponentTypes.MOUSE_COLLISION];
 
       const circleComponents = entity.components(circle, false);
       const rectangleComponents = entity.components(rectangle, false);
+      let detected = false;
       if (circleComponents) {
-        this.detectCircleCollision(circleComponents);
+        detected = this.detectCircleCollision(circleComponents);
       } else if (rectangleComponents) {
-        this.detectRectangleCollision(rectangleComponents);
+        detected = this.detectRectangleCollision(rectangleComponents);
       }
 
+      if (this.mouseDown && detected) {
+        this.entityId = entity.id
+      }
+    } else if (this.entityId == entity.id) {
+      entity.get(ComponentTypes.MOUSE_COLLISION, (c) => {
+        this.doUpdate(c as MouseCollisionComponent)
+      })
     }
   }
 
 
-  private detectCircleCollision(components: Map<ComponentTypes, IComponent>) {
+  private detectCircleCollision(components: Map<ComponentTypes, IComponent>): boolean {
     const pos = components.get(ComponentTypes.POSITION)! as Position;
     const circle = components.get(ComponentTypes.CIRCLE)! as CircleComponent;
     const mouse = components.get(ComponentTypes.MOUSE_COLLISION)! as MouseCollisionComponent;

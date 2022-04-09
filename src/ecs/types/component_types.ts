d7bef401b0505d40a10719926f049bcb84434c89
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:26:43 +0000

    Add more component types
    Add some line comments to help indicate what these components
    are doing

--- src/ecs/types/component_types.ts
+++ src/ecs/types/component_types.ts
@@ -1,12 +1,17 @@
 export enum ComponentTypes {
-  DRAWABLE = 'drawable',
-  FILLABLE = 'fillable',
-  POSITION = 'position',
-  CIRCLE = 'circle',
-  SIZE = 'size',
-  MOUSE_COLLISION = 'mouse_collision',
-  INTERACT = 'interact',
-  LINE = 'line'
+  DRAWABLE = 'drawable',                      // The entity is drawable
+  FILLABLE = 'fillable',                      // The entity can be will with something eg. color
+  POSITION = 'position',                      // The entity can be position on the canvas
+  CIRCLE = 'circle',                          // The entity is a circle
+  CONNECTABLE= 'connectable',                 // The entity can be connected
+  SIZE = 'size',                              // The entity can be sized. note
+  MOUSE_COLLISION = 'mouse_collision',        // The entity response to mouse interaction
+  INTERACT = 'interact',                      // The entity can be interacted with
+  LINE = 'line',                              // The entity is a line
+  LINK = 'link',                              // Stores link data for connector entity
+  CIRCLE_SHAPE_TAG = 'circle_tag',            // Identifies an entity as a `circle` shape
+  RECTANGLE_SHAPE_TAG = 'rectangle_tag',      // Identifies an entity as a `rectangle` shape
+  LINE_SHAPE_TAG = 'line_tag',                // Identifies an entity as a `line` shape
 }
 
 export interface IComponent {

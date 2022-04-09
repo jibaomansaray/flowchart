a6228c56cdb13d2e38d533ad164a0a1ff7c3b458
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:20:44 +0000

    Add more components especially the circle tag

--- src/ecs/entities/circle_entity.ts
+++ src/ecs/entities/circle_entity.ts
@@ -1,10 +1,13 @@
 import { Entity } from ".";
 import { CircleComponent } from "../components/circle";
+import { ConnectableComponent } from "../components/connectable";
 import { DrawableComponent } from "../components/drawable";
 import { FillableComponent } from "../components/fillable";
 import { InteractComponent } from "../components/interact";
 import { MouseCollisionComponent } from "../components/mouse_collision";
 import { Position } from "../components/position";
+import { SizeComponent } from "../components/size";
+import { CircleTagComponent } from "../components/tags/circle_shape";
 import { EntityType } from "../types/entity_types";
 
 export class CircleEntity extends Entity {
@@ -13,11 +16,14 @@ export class CircleEntity extends Entity {
     super(type, id);
 
     this.add(new FillableComponent());
-    this.add(new Position());
+    this.add(new Position(this));
     this.add(new CircleComponent());
     this.add(new MouseCollisionComponent());
     this.add(new DrawableComponent());
     this.add(new InteractComponent(this));
+    this.add(new ConnectableComponent(this));
+    this.add(new SizeComponent(this));
+    this.add(new CircleTagComponent());
   }
 
 }

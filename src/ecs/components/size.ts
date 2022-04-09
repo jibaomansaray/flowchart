2390e11e5c98fd237402c625a9b83d4575d81849
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:19:26 +0000

    Requires the entity
    The entity is use to calculate the width and height in the case
    where such entity does not actually have one. Example: circle.

--- src/ecs/components/size.ts
+++ src/ecs/components/size.ts
@@ -1,11 +1,15 @@
 import { ComponentTypes, IComponent } from "../types/component_types";
+import { IEntity } from "../types/entity_types";
+import { CircleComponent } from "./circle";
 
 export class SizeComponent implements IComponent {
 
   private _width: number;
   private _height: number;
+  private _entity: IEntity;
 
-  constructor(width = 0, height = 0) {
+  constructor(entity: IEntity,width = 0, height = 0) {
+    this._entity = entity;
     this._width = width;
     this._height = height;
   }
@@ -15,25 +19,40 @@ export class SizeComponent implements IComponent {
   }
 
   get width() {
-    return this._width;
+    let width = this._width
+    this._entity.get(ComponentTypes.CIRCLE, (c) => {
+      width = (c as CircleComponent).radius * 2;
+    })
+    return width;
   }
 
   set width(w: number) {
+    this._entity.get(ComponentTypes.CIRCLE, (c) => {
+      (c as CircleComponent).radius = w / 2;
+    })
     this._width = w;
   }
 
   get height() {
-    return this._height;
+    let height = this._height;
+    this._entity.get(ComponentTypes.CIRCLE, (c) => {
+      height = (c as CircleComponent).radius * 2;
+    })
+    return height;
   }
 
   set height(h: number) {
+    this._entity.get(ComponentTypes.CIRCLE, (c) => {
+      (c as CircleComponent).radius = h / 2;
+    })
     this._height = h;
   }
 
   toJSON(): { [key: string]: any; } {
+    // we want to non calculated values. Calculation will be don't at the time of `setting`
     return {
-      width: this.width,
-      height: this.height
+      width: this._width,
+      height: this._height
     };
   }
 

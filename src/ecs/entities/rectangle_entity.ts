3b0d0da624e55831e1549f5581dcfd72bd01e046
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:23:00 +0000

    Add rectangle tag component

--- src/ecs/entities/rectangle_entity.ts
+++ src/ecs/entities/rectangle_entity.ts
@@ -6,16 +6,20 @@ import { SizeComponent } from "../components/size";
 import { MouseCollisionComponent } from "../components/mouse_collision";
 import { DrawableComponent } from "../components/drawable";
 import { InteractComponent } from "../components/interact";
+import { ConnectableComponent } from "../components/connectable";
+import { RectangleTagComponent } from "../components/tags/rectangle_shape";
 
 export class RectangleEntity extends Entity {
 
   constructor(id?: string, type = EntityType.RECTANGLE) {
     super(type, id);
-    this.add(new Position());
+    this.add(new Position(this));
     this.add(new FillableComponent());
-    this.add(new SizeComponent());
+    this.add(new SizeComponent(this));
     this.add(new MouseCollisionComponent());
     this.add(new DrawableComponent());
     this.add(new InteractComponent(this));
+    this.add(new ConnectableComponent(this));
+    this.add(new RectangleTagComponent());
   }
 }

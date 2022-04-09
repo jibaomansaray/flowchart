661f95216e54f6eb2829b08c7426d6f82929d78b
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-09 02:21:58 +0000

    Add the rectangle tag component

--- src/ecs/entities/interact_entity.ts
+++ src/ecs/entities/interact_entity.ts
@@ -2,6 +2,7 @@ import { Entity } from "."
 import { DrawableComponent } from "../components/drawable";
 import { Position } from "../components/position"
 import { SizeComponent } from "../components/size";
+import { RectangleTagComponent } from "../components/tags/rectangle_shape";
 import { EntityType, IEntity } from "../types/entity_types"
 
 export class InteractEntity extends Entity {
@@ -14,9 +15,10 @@ export class InteractEntity extends Entity {
     const drawable = new DrawableComponent();
     drawable.setLineDash(2,1)
 
-    this.add(new Position());
-    this.add(new SizeComponent());
+    this.add(new Position(this));
+    this.add(new SizeComponent(this));
     this.add(drawable);
+    this.add(new RectangleTagComponent());
   }
 
   get child() {

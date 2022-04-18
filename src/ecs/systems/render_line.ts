8945d4c0fa327b396646b5b5f4968ef6dc106616
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-18 07:05:18 +0000

    Add the ability to draw an arrow head

--- src/ecs/systems/render_line.ts
+++ src/ecs/systems/render_line.ts
@@ -24,14 +24,48 @@ export class RenderLine implements ISystem {
     const line = components.get(ComponentTypes.LINE)! as LineComponent;
     const drawable = components.get(ComponentTypes.DRAWABLE)! as DrawableComponent;
 
+    ctx.save()
     ctx.strokeStyle = drawable.strokeStyle || ctx.strokeStyle;
     ctx.setLineDash(drawable.lineDash);
 
     ctx.beginPath();
+    ctx.lineWidth = drawable.lineWidth;
     ctx.moveTo(line.aX, line.aY);
     ctx.lineTo(line.bX, line.bY);
     ctx.stroke();
+
+    if (line.drawHead) {
+      this.arrow(ctx, drawable, line.aX, line.aY, line.bX, line.bY)
+    }
+
+    ctx.restore()
   }
+
+  private arrow(ctx: CanvasRenderingContext2D, drawable: DrawableComponent , x1: number, y1: number, x2: number, y2: number) {
+    ctx.beginPath();
+    ctx.strokeStyle = drawable.strokeStyle || ctx.strokeStyle;
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2);
+    ctx.stroke()
+
+    var dx = x2 - x1
+    var dy = y2 - y1
+    var size = 10
+
+    ctx.save();
+    ctx.strokeStyle = drawable.strokeStyle || ctx.strokeStyle;
+    ctx.fillStyle = drawable.strokeStyle || ctx.strokeStyle
+    ctx.translate(x2, y2)
+    ctx.rotate(Math.atan2(dy, dx))
+    ctx.moveTo(3, 0);
+    ctx.lineTo(-size, -size);
+    ctx.lineTo(-size, size);
+    ctx.closePath();
+    ctx.fill()
+    ctx.restore()
+  }
+
+
 }
 
 export const renderLine = new RenderLine();

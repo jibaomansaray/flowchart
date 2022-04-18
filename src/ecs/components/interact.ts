5955d42fc0388da683b4eefa775356a7f87b472b
Jibao Mansaray <jibaomansaray@gmail.com>
2022-04-18 06:55:38 +0000

    Add `fill` and `highlight` props
    These props set flags that the interaction system
    respects.

--- src/ecs/components/interact.ts
+++ src/ecs/components/interact.ts
@@ -7,11 +7,15 @@ export class InteractComponent implements IComponent {
   private _strokeStyle: string;
   private _fillStyle: string;
   private _box: IEntity;
+  private _highlight: boolean;
+  private _fill: boolean;
 
   constructor(entity: IEntity) {
     this._fillStyle = 'green';
     this._strokeStyle = 'red';
+    this._highlight = true;
     this._box = new InteractEntity(entity);
+    this._fill = true;
 
     manager.addEntity(this._box);
   }
@@ -40,18 +44,43 @@ export class InteractComponent implements IComponent {
     return this._box;
   }
 
+  get highlight() {
+    return this._highlight;
+  }
+
+  set highlight(h: boolean) {
+    this._highlight = h;
+  }
+
+  get fill() {
+    return this._fill;
+  }
+
+  set fill(f: boolean) {
+    this._fill = f;
+  }
+
   toJSON(): { [key: string]: any; } {
     return {
       fillStyle: this.fillStyle,
-      strokeStyle: this.strokeStyle
+      strokeStyle: this.strokeStyle,
+      highlight: this.highlight,
+      fill: this.fill
     }
   }
 
   fromJSON(json: { [key: string]: any; }): void {
-    json = json as { fillStyle: string, strokeStyle: string };
+    json = json as {
+      fillStyle: string,
+      strokeStyle: string,
+      highlight: boolean,
+      fill: boolean
+    };
 
     this.fillStyle = json.fillStyle;
     this.strokeStyle = json.strokeStyle;
+    this.highlight = json.highlight;
+    this.fill = json.fill;
   }
 
 }
